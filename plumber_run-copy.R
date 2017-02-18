416cf7879b893fbe3784c56451392f8de50b6e7f
Daria Maglevanaya <maglevanaya.dary@gmail.com>
2017-02-18 16:17:07 +0300

    .

---
+++ plumber_run-copy.R
@@ -0,0 +1,7 @@
+library(plumber)
+# LOADING SCRIPT
+r <- plumb("backend_example.R")
+# DEFINING GROUP NUMBER
+GROUP_NUMBER = 02
+# THIS WILL BE AVAILABLE AT http://p01.piterdata.ninja
+r$run(port=8100 + GROUP_NUMBER)
